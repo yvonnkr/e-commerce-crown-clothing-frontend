8b680018ae30c76cdf7caf28de510df198bb0f98
Yvonne Nkr <yvonnkr86@gmail.com>
2020-03-22 20:22:29 +0000

    added CartItem comp displayed it in CartDropdown

--- src/components/cart-dropdown/CartDropdown.jsx
+++ src/components/cart-dropdown/CartDropdown.jsx
@@ -1,12 +1,20 @@
 import React from "react";
 
 import CustomButton from "./../custom-button/CustomButton";
+import CartItem from "../cart-item/CartItem";
+import { useSelector } from "react-redux";
 import "./CartDropdown.scss";
 
 const CartDropdown = () => {
+  const cartItems = useSelector(state => state.cart.cartItems);
+
   return (
     <div className={`cart-dropdown`}>
-      <div className="cart-items" />
+      <div className="cart-items">
+        {cartItems.map(cartItem => (
+          <CartItem key={cartItem.id} item={cartItem} />
+        ))}
+      </div>
       <CustomButton>GO TO CHECKOUT</CustomButton>
     </div>
   );
