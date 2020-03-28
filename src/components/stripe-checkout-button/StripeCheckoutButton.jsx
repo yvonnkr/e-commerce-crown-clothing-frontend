0783a83faeb19215891d420db622bb46c3e3470d
Yvonne Nkr <yvonnkr86@gmail.com>
2020-03-28 21:27:58 +0000

    connect to server/backend to make complete payment

--- src/components/stripe-checkout-button/StripeCheckoutButton.jsx
+++ src/components/stripe-checkout-button/StripeCheckoutButton.jsx
@@ -1,14 +1,28 @@
 import React from "react";
 import StripeCheckout from "react-stripe-checkout";
+import axios from "axios";
 
 const StripeCheckoutButton = ({ price }) => {
   const priceForStripe = price * 100;
   const publishableKey = process.env.REACT_APP_STRIPE_PUBLISHABLE_KEY;
 
-  const onToken = token => {
-    console.log(token);
-    alert("Payment Successful.");
+  const onToken = async token => {
+    try {
+      await axios({
+        method: "POST",
+        url: `${process.env.REACT_APP_SERVER_URL}/payment`,
+        data: { token, amount: priceForStripe }
+      });
+
+      alert("Payment successful");
+    } catch (err) {
+      console.log("payment Error", JSON.parse(err));
+      alert(
+        "There was an issue with your payment. Please make sure you use the provided credit card."
+      );
+    }
   };
+
   return (
     <StripeCheckout
       label="Pay Now"
